ab4a9a528f4cb7cc821ccb815e47de168d6d7a5a
george88888 <50836230+george88888@users.noreply.github.com>
2020-04-27 00:28:58 +0100

    Add files via upload

--- clustering-data/main.py
+++ clustering-data/main.py
@@ -21,34 +21,77 @@ def import_data(fname):
 def norms(l, X):
     a = np.size(X, 0)
     norm = np.zeros((a, 1))
+    norm22 = X
     if l == 1: #l1 norm, sum of the absolute values in each feature vector
         for i in range(0, a):
             norm = np.sum(np.abs(X[i,:]))
     elif l == 2: #l2 norm,
         for i in range(0, a):
-            norm[i,:] = np.sqrt(np.sum(X[i,:]*X[i,:]))
+            norm[i, :] = np.sqrt(np.sum(X[i, :]*X[i, :]))
 #            print(i,norm)
 
-
-    elif l == 22: #l2 norm,
-        for i in range(0, np.size(X, 0)):
-            norm = np.sqrt(np.sum(X[i,:]*X[i,:]))
-            X[i,:] = X[i,:] / norm
+    elif l == 22: #l2 norm, normalize
+        for i in range(0, a):
+            norm[i, :] = np.sqrt(np.sum(X[i, :]*X[i, :]))
+            norm22[i, :] = X[i, :] / norm[i, :]
+            print(norm22)
 
     return norm
 
 
-def train():
-    filename = "animals"
-    X, L = import_data(filename)
-    print(X, L)
-    norm = norms(2, X)
+def train(fname):
+
+    X, L = import_data(fname)
+#    print(X, L)
+    norm = norms(22, X)
     print(norm)
+    plt.scatter(X[:, 6], L)
+    plt.show()
     return
 
+def validation():
+
+    return
+
+def test():
+    return
+
+def evaluation():
+    # Precision (P): Precision tells us about all the correct predictions out of total positive predictions
+    # Recall (R): Recall tells us about how many instances were correctly classified out of all positive instances
+    # F-score (F) is the harmonic mean between Precision(P) and Recall (R).
+
+    return
+
+def sim(p, q):
+    score = numpy.dot(p,q) / (numpy.linalg.norm(p) * numpy.linalg.norm(q))
+    return score
+
+def predict(x, k):
+    L = [(y, sim(x, z)) for (y,z) in train_data]
+    L.sort(key=lambda tup: tup[1], reverse = True)  # sorts in place
+    #print L[:k]
+    score = sum([e[0] for e in L[:k]])
+    if score > 0:
+        return 1
+    else:
+        return -1
+
+def accuracy():
+    corrects = 0
+    k = 5
+    for (y,x) in test_data:
+        if y == predict(x, k):
+            corrects += 1
+    accuracy = float(corrects) / float(len(test_data))
+    print ("Accuracy =", accuracy)
+    return accuracy
 
 if __name__ == "__main__":
-    train()
+    train("animals")
+#    train("countries")
+#    train("fruits")
+#    train("veggies")
 
 #    plt.scatter(neg[:, 0], neg[:, 1], c='r')
 #    plt.scatter(pos[:, 0], pos[:, 1], c='b')
